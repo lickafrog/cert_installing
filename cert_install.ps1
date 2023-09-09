be79b46fc27a378d0b865ecb2beeeb8f36adb209
lickafrog <130896496+lickafrog@users.noreply.github.com>
2023-09-09 18:08:44 +0300

    Update cert_install.ps1

--- cert_install.ps1
+++ cert_install.ps1
@@ -10,6 +10,8 @@ $c2 = $env:temp + "\" + "russian_trusted_sub_ca.cer"
 Invoke-WebRequest https://gu-st.ru/content/Other/doc/russian_trusted_root_ca.cer -outfile $c1
 Invoke-WebRequest https://gu-st.ru/content/Other/doc/russian_trusted_sub_ca.cer -outfile $c2
 
+Start-Sleep -s 5
+
 if ((Test-Path $c1) -and (Test-Path $c2)) {
     Import-Certificate -Filepath $c1 -CertStoreLocations = 'Cert:\CurrentUser\Root'
     Import-Certificate -Filepath $c2 -CertStoreLocations = 'Cert:\CurrentUser\Root'
