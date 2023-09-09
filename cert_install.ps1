764723b05d2a31b219970cdaf0eb78d41037b3dc
lickafrog <130896496+lickafrog@users.noreply.github.com>
2023-09-09 18:30:34 +0300

    Update cert_install.ps1

--- cert_install.ps1
+++ cert_install.ps1
@@ -12,8 +12,20 @@ Invoke-WebRequest https://gu-st.ru/content/Other/doc/russian_trusted_sub_ca.cer 
 
 Start-Sleep -s 5
 
+$params1 = @{
+    FilePath = $c1
+    CertStoreLocation = 'Cert:\CurrentUser\Root'
+}
+
+$params2 = @{
+    FilePath = $c2
+    CertStoreLocation = 'Cert:\CurrentUser\Root'
+}
+
 if ((Test-Path $c1) -and (Test-Path $c2)) {
-    Import-Certificate -Filepath $c1 -CertStoreLocation = 'Cert:\CurrentUser\Root'
-    Import-Certificate -Filepath $c2 -CertStoreLocation = 'Cert:\CurrentUser\Root'
+    # Import-Certificate -Filepath $c1 -CertStoreLocation = 'Cert:\CurrentUser\Root'
+    # Import-Certificate -Filepath $c2 -CertStoreLocation = 'Cert:\CurrentUser\Root'
+    Import-Certificate @params1
+    Import-Certificate @params2
     # Write-Output 'exists'
 }
